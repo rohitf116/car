1f4fea45e09d1209bd4b184a1197906053ef3e39
rohitf116 <106576603+rohitf116@users.noreply.github.com>
2023-05-17 18:22:43 +0530

    dtos

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUsersDto } from './dtos/create-users.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUsersDto) {
+    console.log(body);
+  }
+}
