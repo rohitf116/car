af3b7f544882615471db427de09b5fed671efb93
rohitf116 <106576603+rohitf116@users.noreply.github.com>
2023-05-17 12:31:53 +0530

    initail

---
+++ src/reports/reports.entity.ts
@@ -0,0 +1,8 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Reports {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  price: number;
+}
---
+++ src/users/users.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+}
