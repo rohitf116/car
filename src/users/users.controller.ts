988769e13de168f61f59fa059bfe01089f05f37f
rohitf116 <106576603+rohitf116@users.noreply.github.com>
2023-05-18 19:27:53 +0530

    dtos

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,33 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Query,
+  Delete,
+} from '@nestjs/common';
 import { CreateUsersDto } from './dtos/create-users.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUsersDto) {
-    console.log(body);
+    this.userService.create(body.email, body.password);
+  }
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
   }
 }
